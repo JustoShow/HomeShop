c606498f68c67549b9b35189e0f4e99df9db429a
Justoshow <justoproductions@yahoo.com>
2021-06-06 19:42:27 -0500

    Initial Solution Setup

---
+++ HomeShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeShop.WebUI.MvcApplication" Language="C#" %>
---
+++ HomeShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeShop.WebUI.Startup))]
+namespace HomeShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
