966dba38fe2525b1aaf19bd5c7ebc8078bd3f422
Justoshow <justoproductions@yahoo.com>
2021-06-13 07:50:03 -0500

    Added Listing Page

--- HomeShop.WebUI/Controllers/HomeController.cs
+++ HomeShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HomeShop.Core.Contracts;
+using HomeShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,32 @@ namespace HomeShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRepository<Product> _context;
+        private readonly IRepository<ProductCategory> _productCategories;
+
+        public HomeController(IRepository<Product> context, IRepository<ProductCategory> productCategories)
+        {
+            _context = context;
+            _productCategories = productCategories;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = _context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string id)
+        {
+            Product product = _context.Find(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
