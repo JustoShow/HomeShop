9e443bafafde9800b2f6ba760da598e74dba2f15
Justoshow <justoproductions@yahoo.com>
2021-07-01 15:00:10 -0500

    Finished Basket Service

---
+++ HomeShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,21 @@
+﻿using HomeShop.Core.Models;
+using HomeShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HomeShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        Basket CreateNewBasket(HttpContextBase httpContext);
+        Basket GetBasket(HttpContextBase httpContext, bool createIfNull);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+    }
+}
--- HomeShop.Services/BasketService.cs
+++ HomeShop.Services/BasketService.cs
@@ -1,5 +1,6 @@
 ﻿using HomeShop.Core.Contracts;
 using HomeShop.Core.Models;
+using HomeShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ using System.Web;
 
 namespace HomeShop.Services
 {
-    public class BasketService
+    public class BasketService : IBasketService
     {
 
         public const string BasketSessionName = "eCommerceBasket";
@@ -22,7 +23,7 @@ namespace HomeShop.Services
             _basketContext = basketContext;
         }
 
-        private Basket GetBasket(HttpContextBase httpContext, bool createIfNull)
+        public Basket GetBasket(HttpContextBase httpContext, bool createIfNull)
         {
             HttpCookie cookie = httpContext.Request.Cookies.Get(BasketSessionName);
 
@@ -54,7 +55,7 @@ namespace HomeShop.Services
             return basket;
         }
 
-        private Basket CreateNewBasket(HttpContextBase httpContext)
+        public Basket CreateNewBasket(HttpContextBase httpContext)
         {
             Basket basket = new Basket();
             _basketContext.Insert(basket);
@@ -101,6 +102,57 @@ namespace HomeShop.Services
             {
                 basket.BasketItems.Remove(item);
                 _basketContext.Commit();
+            }
+        }
+
+        public List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);
+            
+            if(basket != null)
+            {
+                var result = (from b in basket.BasketItems
+                              join p in _productContext.Collection() on b.ProductId equals p.Id
+                              select new BasketItemViewModel()
+                                          {
+                                              Id = b.Id,
+                                              Quantity = b.Quantity,
+                                              ProcductName = p.Name,
+                                              Image = p.Image,
+                                              Price = p.Price
+                                          }
+                              ).ToList();
+
+                return result;
+            }
+            else
+            {
+                return new List<BasketItemViewModel>();
+            }
+        }
+
+        public BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);
+            BasketSummaryViewModel model = new BasketSummaryViewModel(0, 0);
+
+            if(basket != null)
+            {
+                int? basketCount = (from item in basket.BasketItems
+                                    select item.Quantity).Sum();
+
+                decimal? basketTotal = (from item in basket.BasketItems
+                                        join p in _productContext.Collection() on item.ProductId equals p.Id
+                                        select item.Quantity * p.Price).Sum();
+
+                model.BasketCount = basketCount ?? 0;
+                model.BasketTotal = basketTotal ?? decimal.Zero;
+
+                return model;
+            }
+            else
+            {
+                return model;
             }
         }
     }
